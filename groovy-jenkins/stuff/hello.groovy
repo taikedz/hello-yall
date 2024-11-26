44803ec54c2b010cbe5b957b952a6b18b87f4d0c
Tai Kedzierski <dch.tai@gmail.com>
2024-11-26 14:37:38 +0000

    Update hello.groovy

--- groovy-jenkins/stuff/hello.groovy
+++ groovy-jenkins/stuff/hello.groovy
@@ -2,8 +2,8 @@ def greet(name) {
     return "Hello, ${name.trim()}!"
 }
 
-// The whole file gets encapsulated to a class
-// and any "naked code" goes into a static run() method of the class.
+// The whole file gets encapsulated to a class - each function becomes a method of it,
+// and any "naked code" goes into an instance run() method.
 // We must `return this;` so that the `load` operation receives the loaded instance.
 return this;
 
